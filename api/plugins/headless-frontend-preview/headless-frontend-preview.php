b752d5dc89927732b6b67f00ceb0723298751e24
Paul Gilzow <paul.gilzow@platform.sh>
2022-05-16 16:08:31 -0500

    adds nextjs preview location in .environment adds plugin to filter urls to point previews to the frontend

---
+++ api/plugins/headless-frontend-preview/headless-frontend-preview.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Headless Frontend Preview
+ * Plugin URI: https://platform.sh/
+ * Description: Enables previewing posts
+ * Author: Platform.sh, Paul F. Gilzow
+ * Version: 0.1.0
+ * Requires at least: 4.9.0
+ * Tested up to: 5.9.0
+ * Requires PHP: 7.4
+ * License: GPL-3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+if((null !== $previewURL = getenv('FRONTEND_URL'))
+	&& (null !== $previewSecret = getenv('WORDPRESS_PREVIEW_SECRET'))
+	&& (null !== $previewLocation = getenv('FRONTEND_PREVIEW_LOCATION'))
+)
+{
+	add_filter('preview_post_link', function ($previewLink, $post) use ($previewURL, $previewSecret, $previewLocation){
+		$urlQuery = [
+			'id' 	=> $post->ID,
+			'secret'=> $previewSecret,
+		];
+
+		return $previewURL.$previewLocation.'?'.http_build_query($urlQuery);
+	});
+}
