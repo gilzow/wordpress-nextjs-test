3314b6a5624ecb05dfd149cc1601dab1b982c9d3
Paul Gilzow <paul.gilzow@platform.sh>
2022-05-19 08:54:43 -0500

    initial commit.  verify user exists, if not, creates user account

---
+++ api/platformsh-scripts/environment/01-verify-user.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# @todo verify we got something
+previewUser=$(jq -r '.environment.consumer.name' < "${ENV_SETTINGS}");
+
+# wp user get foobar &> /dev/null;
+
+if ! $(wp user get "${previewUser}" &> /dev/null); then
+	printf "User %s doesn't exist. Creating..." "${previewUser}";
+	. "${WP_SETUP}/project/04-create-user.sh"
+fi
+
+
