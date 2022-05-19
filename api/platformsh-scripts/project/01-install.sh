0bd8a2d9610c749976b548486632f924db8fb512
Paul Gilzow <paul.gilzow@platform.sh>
2022-05-19 09:13:08 -0500

    removes hard coded values, points to values from settings file adds output of site address in console message saves siteurl to settings file

--- api/platformsh-scripts/project/01-install.sh
+++ api/platformsh-scripts/project/01-install.sh
@@ -16,19 +16,25 @@ INIT_ADMIN_PASS=$(echo -n "${PLATFORM_PROJECT_ENTROPY}" | sha1sum | awk '{print 
 # b. Admin email address - we'll have to go with a fake email address and instructions to update it
 # c. Site URL - get that from
 # d. Site Title
-adminName="admin"
-adminEmail="admin@example.com"
-siteTitle="WordPress Headless"
-siteURL=$(echo ${PLATFORM_ROUTES} | base64 --decode | jq -r 'to_entries[] | select(.value.id == "api") | .key')
+adminName=$(jq -r '.project.admin_user.name' < "${ENV_SETTINGS}")
+adminEmail=$(jq -r '.project.admin_user.email' < "${ENV_SETTINGS}")
+siteTitle=$(jq -r '.project.title' < "${ENV_SETTINGS}")
+siteURL=$(echo "${PLATFORM_ROUTES}" | base64 --decode | jq -r 'to_entries[] | select(.value.id == "api") | .key')
 
+#we create our admin user at the same time as we install the site
 wp core install --url="${siteURL}" --title="${siteTitle}" --admin_user="${adminName}" --admin_password="${INIT_ADMIN_PASS}" --admin_email="${adminEmail}"
 # @todo should we check for an error before reporting success?
 
 # 3. Warn the user about the initial admin account.
 printf "    ✔ Installation complete.\n"
+printf "    ✔ Site is available at %s\n" "${siteURL}"
 printf "    ✔ Your WordPress site has been installed with the following credentials:\n"
 printf "        * \033[1muser: \033[0m %s\n" "${adminName}"
 printf "        * \033[1mpass: \033[0m %s\n" "${INIT_ADMIN_PASS}"
 printf "        * \033[1memail:\033[0m %s\n" "${adminEmail}"
 printf "    ✗ \033[1mWARNING: Update your password and email immediately. They will only be available once.\033[0m\n"
 printf "    ✗ \033[1mWARNING: Make sure to update the email address for both the %s user and in Settings --> General.\033[0m\n" "${adminName}"
+
+# Save the site URL
+UPDATED_SETTINGS=$(jq --arg SITEURL "${siteURL}" '.environment.api.url.base = $SITEURL' "$ENV_SETTINGS")
+echo "${UPDATED_SETTINGS}" > "$ENV_SETTINGS"
