82afc7e753270174df17c5284681b6d3b8913598
Paul Gilzow <paul.gilzow@platform.sh>
2022-05-19 08:45:39 -0500

    adds image domain env var that is set by wordpress

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   images: {
     domains: [
-      'api.main-bvxea6i-z6d5mdnrun4va.ca-1.platformsh.site',
+	  // replace this with process.env.NEXT_IMAGE_DOMAIN
+	  process.env.IMAGE_DOMAIN,
+      //'api.main-bvxea6i-z6d5mdnrun4va.ca-1.platformsh.site',
       'secure.gravatar.com',
     ],
   },
